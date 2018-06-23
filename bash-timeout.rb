f7b785f9b2502651fc2b6f8ab39c237ec5abbe9b
Takahide Nogayama <nogayama@jp.ibm.com>
2018-06-23 15:04:20 +0900

    feat: Add bash-timeout.rb

---
+++ bash-timeout.rb
@@ -0,0 +1,13 @@
+class BashTimeout < Formula
+  desc "A command (and also bash function) to terminate another command after specified duration"
+  homepage "https://github.com/nogayama/bash-timeout"
+
+  url "https://github.com/nogayama/bash-timeout.git", :tag => "v1.1.1"
+
+  def install
+    bin.install "bin/bash-timeout"
+  end
+  test do
+    system "false"
+  end
+end
