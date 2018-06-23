53f09eeaa67faa9b3b676270aab58c7619fdaffe
Takahide Nogayama <nogayama@jp.ibm.com>
2018-06-23 16:21:29 +0900

    Update to v1.1.1

--- bash-timeout.rb
+++ bash-timeout.rb
@@ -1,17 +1,14 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
 class BashTimeout < Formula
   desc "A command (and also bash function) to terminate another command after specified duration"
   homepage "https://github.com/nogayama/bash-timeout"
-
-  #url "https://github.com/nogayama/bash-timeout.git", :tag => "v1.1.1"
-  # head "https://github.com/nogayama/bash-timeout.git"
-  url "https://github.com/nogayama/bash-timeout/archive/v1.1.1.tar.gz"
-  
   version "1.1.1"
+
+  url "https://github.com/nogayama/bash-timeout/archive/v1.1.1.tar.gz"
   
   def install
     bin.install "bin/bash-timeout"
-  end
-  test do
-    # system "false"
   end
 end
